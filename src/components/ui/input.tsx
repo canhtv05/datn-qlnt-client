9e3dd9362cd543ecb55bcb9f5c436ed8cf898106
canhtv05 <canhtvph51036@gmail.com>
2025-06-01 20:19:23 +0700

    page(login): add login page

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,50 @@
-import * as React from "react"
+import * as React from "react";
+import { CircleAlert } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import RenderIf from "../RenderIf";
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+function Input({ className, type, validate, ...props }: React.ComponentProps<"input"> & { validate?: boolean }) {
+  const [isEmpty, setIsEmpty] = React.useState<boolean>(false);
+  const typeIsPassword = type === "password";
+
+  const handleBlur = (value: string) => {
+    if (!value.trim() || value.startsWith(" ")) {
+      setIsEmpty(true);
+    }
+  };
+
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
-      )}
-      {...props}
-    />
-  )
+    <>
+      <div className="relative">
+        <input
+          type={type}
+          data-slot="input"
+          className={cn(
+            "file:text-foreground placeholder:text-foreground/50 dark:bg-input/30 border-[oklch(1 0 0 / 15%)] flex h-9 w-full min-w-0 rounded-sm border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "focus-visible:border-primary focus-visible:ring-primary/50 focus-visible:ring-[0.5px]",
+            "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+            "focus-visible:transform focus-visible:placeholder:translate-x-[2px] not-focus-visible:placeholder:-translate-x-[2px] focus-visible:placeholder:transition-transform focus-visible:placeholder:duration-300 not-focus-visible:placeholder:duration-300",
+            "focus-visible:shadow-lg placeholder:pl-1",
+            validate && isEmpty && "border-red-500",
+            className
+          )}
+          onBlur={(e) => handleBlur(e.target.value)}
+          onFocus={() => setIsEmpty(false)}
+          {...props}
+        />
+        <RenderIf value={isEmpty}>
+          <CircleAlert className="absolute right-2 top-1/2 -translate-y-1/2 size-[14px] stroke-red-500" />
+        </RenderIf>
+        <RenderIf value={typeIsPassword}>
+          <CircleAlert className="absolute right-2 top-1/2 -translate-y-1/2 size-[14px] stroke-red-500" />
+        </RenderIf>
+      </div>
+      <RenderIf value={isEmpty}>
+        <span className="text-[12px] text-red-500 font-light">Thông tin bắt buộc</span>
+      </RenderIf>
+    </>
+  );
 }
 
-export { Input }
+export { Input };
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,13 +1,72 @@
+import { Link } from "react-router-dom";
+
 import { svg } from "@/assets/svg";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import RenderIf from "@/components/RenderIf";
+import { Viewport } from "@/enums";
+import { useLogin } from "./useLogin";
 
 const Login = () => {
+  const { width, handleSubmitForm, value, setValue } = useLogin();
+
   return (
-    <main className="flex w-full h-screen">
-      <article className="p-5 flex justify-center items-center bg-primary flex-1 float-start w-[70%]">
-        <img src={svg.login} alt="img_login" className="object-contain h-[80%]" loading="lazy" />
-      </article>
-      <aside className="flex flex-col  float-end w-[35%] bg-white">
-        <img src={svg.react} alt="logo" className="object-contain w-[80px] h-[80px]" loading="lazy" />
+    <main className="lg:flex w-full h-screen justify-center">
+      <RenderIf value={width >= Viewport.LG}>
+        <article className="p-5 flex justify-center items-center bg-primary flex-1 float-start w-[70%]">
+          <img src={svg.login} alt="img_login" className="object-contain h-[80%]" loading="lazy" />
+        </article>
+      </RenderIf>
+      <aside className="flex justify-center items-center h-full float-end lg:w-[35%] w-full bg-white lg:px-12 py-15 md:px-55 px-15">
+        <div className="flex items-center h-full flex-col w-full justify-center">
+          <img src={svg.react} alt="logo" className="object-contain w-[100px] h-[100px]" loading="lazy" />
+          <h3 className="text-[#6e6b7b] text-left w-full text-[24px] mt-2">Đăng nhập! 👋</h3>
+          <p className="text-[#6e6b7b] mb-2 text-[14px] text-left w-full mt-2">
+            Bất động sản, tài chính, khách hàng... và hơn thế nữa
+          </p>
+          <form className="w-full mt-5" onSubmit={(e) => handleSubmitForm(e)}>
+            <Label htmlFor="email" className="mb-2 text-[#6e6b7b]">
+              Email:
+            </Label>
+            <Input
+              value={value.email}
+              validate
+              id="email"
+              type="text"
+              placeholder="0987645862"
+              className="mt-0.5 text-[#6e6b7b]"
+              onChange={(e) => setValue((prev) => ({ ...prev, email: e.target.value }))}
+            />
+            <div className="mt-4">
+              <Label htmlFor="pw" className="mb-2 text-[#6e6b7b]">
+                Mật khẩu
+              </Label>
+              <Input
+                value={value.password}
+                validate
+                id="pw"
+                type="text"
+                placeholder="Mật khẩu"
+                className="mt-0.5 text-[#6e6b7b"
+                onChange={(e) => setValue((prev) => ({ ...prev, password: e.target.value }))}
+              />
+            </div>
+            <Link to={``} className="w-full text-right block mt-3 mb-6">
+              <span className="text-primary text-[13px]">Quên mật khẩu?</span>
+            </Link>
+
+            <Button type="submit" className="w-full" disabled={!value.email.trim() || !value.password.trim()}>
+              <span className="text-white">Đăng nhập</span>
+            </Button>
+          </form>
+          <span className="mt-5 text-[14px] text-[#6e6b7b]">
+            Bạn chưa có tài khoản?
+            <Link to={``} className="ml-2 text-primary">
+              Đăng ký miễn phí
+            </Link>
+          </span>
+        </div>
       </aside>
     </main>
   );
