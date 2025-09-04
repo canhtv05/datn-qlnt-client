f108aba99309ceaa5ba8048cb74c1d7f9462672f
NgheSiGia <163287898+NgheSiGia@users.noreply.github.com>
2025-09-04 22:11:20 +0700

    update: update translate english i18n

--- src/components/CccdUpload.tsx
+++ src/components/CccdUpload.tsx
@@ -119,7 +119,7 @@ export default function CccdUpload({
       setFrontUrl(null);
       setFrontResult(null);
     }
-  }, [frontImage]);
+  }, [callOcrApi, frontImage]);
 
   useEffect(() => {
     if (backImage instanceof File) {
@@ -132,7 +132,7 @@ export default function CccdUpload({
       setBackUrl(null);
       setBackResult(null);
     }
-  }, [backImage]);
+  }, [backImage, callOcrApi]);
 
   useEffect(() => {
     if (Array.isArray(frontResult) && frontResult.length > 0) {
